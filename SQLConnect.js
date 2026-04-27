31d4729acc0619ae02c242978d430b33be5fedf4
戴辉 <16206724@gitlab.com>
2026-04-27 19:39:46 +0800

    更新数据库连接配置

--- SQLConnect.js
+++ SQLConnect.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 const MySQLObj = {
     host:"127.0.0.1",
-	port: 3306,      
+	port: 3306,
     user:"root",
-    password:"123456",
-    database:"bzshop"
+    password:"Dh@2026shop",
+    database:"shop"
 }
 
 const pool = mysql.createPool(MySQLObj)
